8ef6ed3ec6fca9f13f6bbbff52cdd20688a97ba1
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-04-27 23:57:26 -0300

    react-unicheck#20-ndm-maquetar_vista_login
    Version 1: Se maqueto el login

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Home from './components/Home/Home';
 import {Provider} from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import { middleware as reduxPackMiddleware } from 'redux-pack'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import thunk from 'redux-thunk'
 import subjectsReducer from './reducers/index'
+import Login from "./components/Login/Login";
 
 let store = createStore(subjectsReducer, applyMiddleware(thunk, reduxPackMiddleware));
 
 function App() {
   return (
-    
     <Provider store = {store}>
-      <Home />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />}/>
+                <Route path="home" element={<Home />} />
+            </Routes>
+        </Router>
     </Provider>
     
   );
