428aee61811265b201efe8aaaba9cd3ccc5bf615
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-05-12 17:39:01 -0300

    Notificación de error en registro

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,47 +1,76 @@
-import './Register.scss'
-import {useState} from "react";
-import PrimaryButton from "../PrimaryButton/PrimaryButton";
+import './Register.scss';
+import { useState } from 'react';
+import PrimaryButton from '../PrimaryButton/PrimaryButton';
 import { useNavigate } from 'react-router';
 import Api from '../../Api';
+import ErrorAlert from '../ErrorAlert/ErrorAlert';
 
 const Register = () => {
-    const api = new Api()
-    const navigate = useNavigate()
-    const [data, setData] = useState({username: "", password: ""});
+  const api = new Api();
+  const navigate = useNavigate();
+  const [data, setData] = useState({ username: '', password: '' });
 
-    const handleChange = (propName) => (event) =>{
-        setData(prevState => ({...prevState, [propName]: event.target.value}))
+  const [error, setError] = useState(null);
+
+  const handleChange = (propName) => (event) => {
+    setError(null);
+    setData((prevState) => ({ ...prevState, [propName]: event.target.value }));
+  };
+
+  const isValidData = () => {
+    if (!data.password || data.password.length < 6) {
+      setError('La contraseña debe tener al menos 6 caracteres.');
+      return false;
     }
+    return true;
+  };
 
-    const handleClick = (event) => {  
-        event.preventDefault();
-        
-        api.post('api/v1/authenticate/students/register', data).
-            then(res => {
-                console.log("la respuesta en el registro es:", res)
-                api.agregarToken(res.id);
-            }).then(() => navigate("/home")).
-            catch(() => {
-            });        
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (isValidData()) {
+      api
+        .post('api/v1/authenticate/students/register', data)
+        .then((res) => {
+          api.agregarToken(res.id);
+        })
+        .then(() => navigate('/home'))
+        .catch((exception) => {
+          console.log(exception);
+          if (exception.status === 409) {
+            setError('El nombre de usuario elegido ya se encuentra en uso.');
+          }
+        });
     }
-    return (
-        <div className='login-view'>
-            <div className='login-container'>
-                <h1>Crear un nuevo usuario</h1>
-                <label>Nombre de usuario</label>
-                <input className='login-input' value={data.username}
-                       onChange={handleChange("username")}/>
+  };
 
-                <label>Contraseña</label>
-                <input className='login-input' type='password' value={data.password}  onChange={handleChange("password")}/>
+  return (
+    <div className='login-view'>
+      <div className='login-container'>
+        <h1>Crear un nuevo usuario</h1>
+        <label>Nombre de usuario</label>
+        <input
+          className='login-input'
+          value={data.username}
+          onChange={handleChange('username')}
+        />
 
-                <div className='login-buttons'>
-                    <PrimaryButton handleClick={handleClick}>Registrarse</PrimaryButton>
-                    <PrimaryButton handleClick={() => navigate('/')}>Login</PrimaryButton>
-                </div>
-            </div>
+        <label>Contraseña</label>
+        <input
+          className='login-input'
+          type='password'
+          value={data.password}
+          onChange={handleChange('password')}
+        />
+
+        {error ? <ErrorAlert message={error} /> : null}
+
+        <div className='login-buttons'>
+          <PrimaryButton handleClick={handleClick}>Registrarse</PrimaryButton>
+          <PrimaryButton handleClick={() => navigate('/')}>Login</PrimaryButton>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
