6678c22737fba517603e02422124440adc699c27
Gonzalo Cortave <gonzalocortave@amalgama.co>
2022-05-14 20:37:08 -0300

    Close uc-52 - Notificar feedback de errores materia en Editar Materia

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,10 +1,12 @@
 import './input.scss';
+import ErrorAlert from '../ErrorAlert/ErrorAlert'
 
-const Input = ({children, handleChange, value, type="text", disabled}) => {
+const Input = ({children, handleChange, value, type="text", disabled, showError=false, errorMessage=""}) => {
     return(
         <div className="Input">
             <label>{children}</label>
             <input disabled={disabled} onChange={handleChange} value={value} type={type}/>
+            {showError ? <ErrorAlert message={errorMessage} /> : null }
         </div>
     )
 }
